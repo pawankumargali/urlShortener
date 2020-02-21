b20346362ca6ed2e8da4ef2f752ddb12c56dd585
pawankumargali <pawankumargali@gmail.com>
2020-02-22 00:26:14 +0530

    Uninstalled body-parser and set views folder

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // MODULES
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const dbConnect = require('./dbConnect');
 
 // SERVER CONFIG
 const app = express();
 app.set('view engine','ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.json());
 
 // DBCONNECT

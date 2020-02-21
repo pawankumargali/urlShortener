54d1d098095a419adb8f0648260f808b7a732d31
pawankumargali <pawankumargali@gmail.com>
2020-02-22 00:33:16 +0530

    added path module, renamed index.html to index.ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const dbConnect = require('./dbConnect');
+const path = require('path');
 
 // SERVER CONFIG
 const app = express();
